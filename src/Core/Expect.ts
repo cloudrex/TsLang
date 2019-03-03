f0f1960a87024ee1f22678cca83cca921f79569a
CloudRex <cloudrex@outlook.com>
2019-03-02 22:39:35 -0500

    Implement expect decorator functionality

--- src/Core/Expect.ts
+++ src/Core/Expect.ts
@@ -4,11 +4,18 @@ export enum Type {
     Number = "number"
 }
 
-export function expect(...type: Array<Type | any>): any {
-    return function (target: any) {
-        // TODO
-        console.log(target);
+export function expect(...types: Array<Type | any>): any {
+    return function (target: any, prop: string) {
+        // Verify all provided types.
+        for (const type of types) {
+            if (Type[type] === undefined) {
+                throw new Error("An invalid type was provided");
+            }
+            else if (typeof target[prop] !== type) {
+                throw new Error(`Expected parameter '${prop}' to be of type '${type}' but got '${typeof target[prop]}' instead`);
+            }
+        }
 
-        return target as any;
+        return target;
     }
 }
